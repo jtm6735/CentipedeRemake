1846ebdc9b1555157ea8d72eed2c8158860d58b6
Zach Harwood (RIT Student) <zah8756@ad.rit.edu>
2018-03-21 16:18:52 -0400

    put in a folder

--- utilities.js
+++
@@ -1,9 +0,0 @@
-export {getMouse};
-
-function getMouse(e){
-    var mouse = {};
-    mouse.x = e.pageX - e.target.offsetLeft;
-    mouse.y = e.pageY - e.target.offsetWidth;
-    console.log("used");
-    return mouse;
-}
