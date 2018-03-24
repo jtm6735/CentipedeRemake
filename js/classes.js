f8746bd483082db58d7cff1f254f901db5784114
zach harwood <31451701+zah8756@users.noreply.github.com>
2018-03-24 13:06:33 -0400

    fixed sruff

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,5 @@
 "use strict";
-export{createMushroomSprites,createPlayerSprite};
+export{createMushroomSprites,createPlayerSprite,fireBullets};
 class character{
     constructor(x,y,fwd,speed){
             // properties
@@ -58,6 +58,31 @@ class player{
     }
 }
 
+class bullets{
+    constructor(x,y,width,height,speed,image){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        //this.color=color;
+        this.speed=speed;
+        this.image=image;
+    	// other properties
+		this.dx = 0; // per second
+        this.dy = 0; // per second 
+    }
+    update(dt){
+		this.x += this.dx * dt;
+		this.y += this.dy * dt;
+    }
+    draw(ctx){
+        ctx.save();
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.restore();
+    }
+    
+}
+
 
 function createMushroomSprites(num,rect = {left: 0,top: 0,width: 300,height: 300},radius,color){
      let sprites = [];
@@ -78,6 +103,12 @@ function createPlayerSprite(rect={left: 0,top: 0,width: 300,height: 300},width,h
     return ship;
 }
 
+function fireBullets(rect={left: 0,top: 0,width: 300,height: 300},x,y,speed,url){
+    let image = new Image();
+    image.src = url; 
+    let bullet = new player(x,y,50,50,speed,image);
+    return bullet;
+}
 
 
 
